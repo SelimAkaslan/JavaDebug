34d0fac0a62e2e0eccde6287f811110a820f91c6
selim akaslan <sakaslan6957@gmail.com>
2021-08-18 01:16:03 +0200

    2. commit

--- src/debugTutorial/Debug01.java
+++ src/debugTutorial/Debug01.java
@@ -1,12 +1,14 @@
 package debugTutorial;
 
 public class Debug01 {
+    static int global =9;
     public static void main(String[] args) {
         dongu1();
 
     }
 
     public static void dongu1() {
+        global++;
         for (int i = 1; i <= 10; i++) {
             System.out.print(i + " ");
         }
@@ -15,12 +17,14 @@ public class Debug01 {
     }
 
     public static void dongu2() {
+        global+=3;
         for (int x = 1; x <= 5; x++) {
             System.out.print(x + " ");
         }
     }
 
     public static void dongu3() {
+        global-=1;
         for (int y = 1; y <= 2; y++) {
             System.out.print(y + " ");
         }
